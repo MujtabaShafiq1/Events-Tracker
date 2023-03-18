f56118e3942ab25d36529ff44fb7dc094ca04b16
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-03-19 00:02:12 +0500

    optimization added

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,5 +1,6 @@
 import { Fragment, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import useSWR from "swr";
 
 import { getFilteredEvents } from "../../helpers/api-util";
@@ -33,8 +34,20 @@ function FilteredEventsPage() {
         }
     }, [data]);
 
+    let pageHeadData = (
+        <Head>
+            <title>Filtered Events</title>
+            <meta name="description" content={`A list of filtered events.`} />
+        </Head>
+    );
+
     if (!loadedEvents) {
-        return <p className="center">Loading...</p>;
+        return (
+            <Fragment>
+                {pageHeadData}
+                <p className="center">Loading...</p>
+            </Fragment>
+        );
     }
 
     const filteredYear = filterData[0];
@@ -43,9 +56,17 @@ function FilteredEventsPage() {
     const numYear = +filteredYear;
     const numMonth = +filteredMonth;
 
+    pageHeadData = (
+        <Head>
+            <title>Filtered Events</title>
+            <meta name="description" content={`All events for ${numYear}/${numMonth}`} />
+        </Head>
+    );
+
     if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12 || error) {
         return (
             <Fragment>
+                {pageHeadData}
                 <ErrorAlert>
                     <p>Invalid filter. Please adjust your values!</p>
                 </ErrorAlert>
@@ -64,6 +85,7 @@ function FilteredEventsPage() {
     if (!filteredEvents || filteredEvents.length === 0) {
         return (
             <Fragment>
+                {pageHeadData}
                 <ErrorAlert>
                     <p>No events found for the chosen filter!</p>
                 </ErrorAlert>
@@ -78,6 +100,7 @@ function FilteredEventsPage() {
 
     return (
         <Fragment>
+            {pageHeadData}
             <ResultsTitle date={date} />
             <EventList items={filteredEvents} />
         </Fragment>
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
+import Head from "next/head";
+
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
@@ -17,6 +19,10 @@ function EventDetailPage(props) {
 
     return (
         <Fragment>
+            <Head>
+                <title>{event.title}</title>
+                <meta name="description" content={event.description} />
+            </Head>
             <EventSummary title={event.title} />
             <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
             <EventContent>
@@ -46,6 +52,6 @@ export async function getStaticPaths() {
 
     return {
         paths: paths,
-        fallback: 'blocking',
+        fallback: "blocking",
     };
 }
